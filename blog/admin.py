ac4012e0a3a2da0684bfc815464e5b3787de7898
subedibiraj <subedi.biraj655@gmail.com>
2024-06-26 15:18:16 +0545

    created model, superadmin and understood crud operation

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from blog.models import category
 
 # Register your models here.
+admin.site.register(category)
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from blog.models import category
 
 # Create your views here.
 
 def home(request):
-    return render(request,'index.html')
+    categories= category.objects.all()
+    context = {'categories':categories}
+    return render(request,'index.html',context)
